37b7969f451c646f6a2c66bf09a530e138390e38
Maartyl <kuronomaartyl@gmail.com>
2015-10-18 22:47:09 +0200

    init commit

---
+++ gen_autokey.py
@@ -0,0 +1,163 @@
+import sys
+import functools
+import json
+from copy import deepcopy
+
+def template_script(replacement):
+    return """
+keyboard.send_keys("<ctrl>+<shift>+u%s")
+keyboard.send_keys("<ctrl>")
+    """ % replacement
+
+    # """
+    # import time
+    # clip = clipboard.get_clipboard()
+
+    # clipboard.fill_clipboard(u"<placeholder>")
+    # keyboard.send_keys("<ctrl>+v")
+
+    # time.sleep(0.05)
+    # clipboard.fill_clipboard(clip)
+    # """
+
+t_json = {'filter': {'regex': None, 'isRecursive': False},
+          'omitTrigger': False,
+          'store': {},
+          'abbreviation': {'ignoreCase': False,
+                           'wordChars': '[\\w]',
+                           'backspace': True,
+                           'immediate': True,
+                           'abbreviations': [],  # add abbrev to this list
+                           'triggerInside': False},
+          'type': 'script',
+          'prompt': False,
+          'modes': [1],
+          'usageCount': 0,
+          'showInTrayMenu': False,
+          'description': '',
+          'hotkey': {'modifiers': [], 'hotKey': None}}
+
+def template_json(abbrev):
+    z = deepcopy(t_json)
+    z["abbreviation"]["abbreviations"].append(abbrev)
+    # extra changes to tree
+    return json.dumps(z)
+
+encode_file_name_dict = {
+    '`': 'backtick_',
+    '/': 'slash_',
+    '\\': 'backslash_',
+    '|': 'pipe_',
+    '_': 'underscore_',
+    '&': 'and_',
+    '+': 'plus_',
+    '-': 'minus_',
+    ',': 'comma_',
+    ';': 'semicolon_',
+}
+
+def encode_file_name(abbrev):
+    l = []
+    for c in abbrev:
+        l.append(encode_file_name_dict.get(c, c))
+    return ''.join(l)
+
+def file_push(fname, data):
+    with open(fname, "w") as f:
+        f.write(data)
+
+def template_run(abbrev, replacement):
+    # h = "\\U" + hex(ord(replacement))[2:].zfill(8)  # char -> num -> hex to 8 places
+    h = hex(ord(replacement))[2:]  # char -> num -> hex (without 0x)
+
+    fname = encode_file_name(abbrev)
+    fname_json = '.' + fname + '.json'
+    fname_script = fname + '.py'
+
+    print(abbrev, h, fname_json, fname_script)
+
+    file_push(fname_json, template_json(abbrev))
+    file_push(fname_script, template_script(h))
+
+def char_range(start, end):
+    """ a..z == char_range('a', 'z') """
+    return map(chr, range(ord(start), ord(end) + 1))
+
+def chars(src):
+    if len(src) == 4 and src[1:3] == '..':
+        return char_range(src[0], src[3])
+    else:
+        return src.split()
+
+# convention: rules :: line -> (abbrev, replacement)
+
+def rule_exact(line):
+    ls = line.split()
+    if len(ls) != 2:
+        raise Exception("incorrect line for exact_rule: " + line)
+    return ls[1], ls[0]
+
+def rule_combine(fmtr, line):
+    ls = line.split()
+    if len(ls) != 2:
+        raise Exception("incorrect line for combine_rule: " + line)
+    return fmtr(ls[1]), ls[0]
+
+def rule_gen_zip(fmtr, names):
+    # print([a for a in names])
+    gen = iter(names)
+
+    def rule(line):
+        return fmtr(next(gen)), line.strip()
+    return rule
+
+def formatter(fmt):
+    def replace(char):
+        return fmt.replace('_', char)
+    return replace
+
+def processor_rule(line):
+    lr = line.split(';', 1)
+    l = lr[0].strip()
+    r = None
+    if len(lr) == 2:
+        r = lr[1].strip()
+
+    ls = l.split()
+    # print(lr, l, r, ls)
+    if r is None:
+        if len(ls) == 1:
+            if ls[0] == ':exact':
+                return rule_exact
+            else:
+                return functools.partial(rule_combine, formatter(ls[0]))
+    else:
+        if len(ls) == 1:
+            return rule_gen_zip(formatter(ls[0]), chars(r))
+    return None
+
+def processor(line):
+    rule = processor_rule(line[1:])
+    if rule is None: raise Exception("rule not matched: " + line)
+    return rule
+
+def proc_err(line):
+    raise Exception("no rule before data")
+
+def remove_comment_norm(line):
+    """ remove comment (#...) from line  and normalize (strip)"""
+    return line[:line.find('#')].strip()
+
+def nonempty(x):
+    return x != ""
+
+def main_loop(lines):
+    p = proc_err
+    for line in filter(nonempty, map(remove_comment_norm, lines)):
+        if line[0] == '$':
+            p = processor(line)
+        else:
+            template_run(*p(line))
+
+if __name__ == "__main__":
+    main_loop(sys.stdin)
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SRCPATH="$HOME/Dropbox/Dev/math/kate-greek-snippets"
+
+cd "$HOME/.config/autokey/data/My Phrases/math-gen"
+
+# later improve: not delete folder file
+rm * .*
+
+python3 "$SRCPATH/gen_autokey.py" < "$SRCPATH/list.md"
